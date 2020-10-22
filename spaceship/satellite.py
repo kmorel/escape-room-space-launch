b29770ae22c2968637d2435e667f022aea85c73a
Kenneth Moreland <kmorel@sandia.gov>
2020-10-21 20:24:38 -0600

    Blink pattern

--- spaceship/satellite.py
+++ spaceship/satellite.py
@@ -4,7 +4,23 @@ import direct.task.TaskManagerGlobal
 
 import panda3d_utils
 
+import enum
 import random
+
+class Code:
+    DOT = enum.auto()
+    DASH = enum.auto()
+    END_LETTER = enum.auto()
+
+message = [
+    Code.DOT, Code.DOT, Code.DOT,  # S
+    Code.END_LETTER,
+    Code.DASH,                     # T
+    Code.END_LETTER,
+    Code.DOT, Code.DASH,           # A
+    Code.END_LETTER,
+    Code.DOT, Code.DASH, Code.DOT, # R
+]
 
 class Satellite:
     def __init__(self, base):
@@ -32,18 +48,49 @@ class Satellite:
         self.node.setTexture(self.texture_off)
 
         self.pause_dot = 0.25
-        self.pause_dash = 0.75
-        self.pause_between = 0.5
-        self.pause_end = 4
+        self.pause_dash = 1
+        self.pause_between_d = 0.5
+        self.pause_end_letter = 1
+        self.pause_end = 6
 
-        direct.task.TaskManagerGlobal.taskMgr.add(self.between_task, 'light')
+        self.code_location = 0
 
-    def between_task(self, task):
+        direct.task.TaskManagerGlobal.taskMgr.add(self.between_d_task, 'light')
+
+    def between_d_task(self, task):
         self.node.setTexture(self.texture_off)
-        if task.time < self.pause_between:
+        if task.time < self.pause_between_d:
             return direct.task.Task.cont
         else:
-            direct.task.TaskManagerGlobal.taskMgr.add(self.dash_task, 'light')
+            if self.code_location < len(message):
+                tasks = {
+                    Code.DOT: self.dot_task,
+                    Code.DASH: self.dash_task,
+                    Code.END_LETTER: self.between_letter_task,
+                }
+                direct.task.TaskManagerGlobal.taskMgr.add(
+                    tasks[message[self.code_location]], 'light')
+                self.code_location += 1
+            else:
+                direct.task.TaskManagerGlobal.taskMgr.add(
+                    self.end_task, 'light')
+                self.code_location = 0
+            return direct.task.Task.done
+
+    def between_letter_task(self, task):
+        self.node.setTexture(self.texture_off)
+        if task.time < self.pause_end_letter:
+            return direct.task.Task.cont
+        else:
+            direct.task.TaskManagerGlobal.taskMgr.add(self.between_d_task, 'light')
+            return direct.task.Task.done
+
+    def end_task(self, task):
+        self.node.setTexture(self.texture_off)
+        if task.time < self.pause_end:
+            return direct.task.Task.cont
+        else:
+            direct.task.TaskManagerGlobal.taskMgr.add(self.between_d_task, 'light')
             return direct.task.Task.done
 
     def dot_task(self, task):
@@ -51,7 +98,7 @@ class Satellite:
         if task.time < self.pause_dot:
             return direct.task.Task.cont
         else:
-            direct.task.TaskManagerGlobal.taskMgr.add(self.between_task, 'light')
+            direct.task.TaskManagerGlobal.taskMgr.add(self.between_d_task, 'light')
             return direct.task.Task.done
 
     def dash_task(self, task):
@@ -59,5 +106,5 @@ class Satellite:
         if task.time < self.pause_dash:
             return direct.task.Task.cont
         else:
-            direct.task.TaskManagerGlobal.taskMgr.add(self.between_task, 'light')
+            direct.task.TaskManagerGlobal.taskMgr.add(self.between_d_task, 'light')
             return direct.task.Task.done
